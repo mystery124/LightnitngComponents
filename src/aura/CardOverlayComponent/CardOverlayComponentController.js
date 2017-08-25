e8d2363774914eb092b9edb18d41c07d47280edc
Stefan Abramiuk <s.abramiuk@polsource.com>
2017-08-25 13:25:27 +0200

    Spinner functionality

---
+++ src/aura/CardOverlayComponent/CardOverlayComponentController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Stefan Abramiuk on 25.08.2017.
+ */
+({
+    showSpinner : function(cmp, event, helper){
+        helper.showSpinner(cmp);
+    },
+    hideSpinner : function(cmp, event, helper){
+        helper.hideSpinner(cmp);
+    },
+})
